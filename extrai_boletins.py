6f26def0f4263abbf534c5ae42ab329ae9e6d94f
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2018-02-22 13:12:21 -0300

    Adiciona scrapers

---
+++ extrai_boletins.py
@@ -0,0 +1,72 @@
+import csv
+import io
+import re
+import logging
+from pathlib import Path
+
+import rows
+from scrapy import Request, Spider
+
+
+logging.getLogger("pdfminer").setLevel(logging.ERROR)
+regexp_costa = re.compile('[ -]([A-Z]{3,4})[ -]')
+
+
+def clean(text):
+    return text.replace('\n', ' ').strip()
+
+
+def extrai_costa(ponto):
+    """
+    >>> extrai_costa('2ª. Praia de Morro de São Paulo - CDD- SP 200')
+    'CDD'
+    >>> extrai_costa('Costa - Canavieiras - CCA CN 100')
+    'CCA'
+    >>> extrai_costa('Madre de Deus - BTS MD 100')
+    'BTS'
+    >>> extrai_costa('Nativos - CDES NT 100')
+    'CDES'
+    """
+
+    return regexp_costa.findall(ponto)[0]
+
+
+class ExtraiBoletins(Spider):
+    name = 'inema-balneabilidade-extrai-boletins'
+
+    def start_requests(self):
+        download_path = Path('./download')
+        if not download_path.exists():
+            download_path.mkdir()
+
+        with open('output/boletins.csv', encoding='utf8') as fobj:
+
+            for row in csv.DictReader(fobj):
+                filename = download_path / f'{row["id_campanha"]}.pdf'
+                row['filename'] = filename
+                if filename.exists():
+                    url = 'file://' + str(filename.absolute())
+                else:
+                    url = row['url']
+
+                yield Request(url=url, meta=row)
+
+    def parse(self, response):
+        meta = response.request.meta.copy()
+        filename = meta['filename']
+        meta['costa_menu'] = meta['costa']
+        del_keys = [key for key in meta.keys()
+                    if key.startswith('download_')
+                    or key in ('url', 'filename', 'depth', 'costa')]
+        for key in del_keys:
+            del meta[key]
+        with open(filename, mode='wb') as fobj:
+            fobj.write(response.body)
+
+        for row in rows.import_from_pdf(io.BytesIO(response.body)):
+            row = row._asdict()
+            row['local_da_coleta'] = clean(row['local_da_coleta'])
+            row['ponto_codigo'] = clean(row['ponto_codigo'])
+            row['costa_ponto'] = extrai_costa(row['ponto_codigo'])
+            row.update(meta)
+            yield row
---
+++ lista_boletins.py
@@ -0,0 +1,30 @@
+from scrapy import Request, Spider
+
+
+URL_BOLETIM = 'http://balneabilidade.inema.ba.gov.br/index.php/relatoriodebalneabilidade/geraBoletim?idcampanha={id_campanha}'
+
+
+class ListaBoletinsSpider(Spider):
+    name = 'inema-balneabilidade-lista-boletins'
+    start_urls = ['http://balneabilidade.inema.ba.gov.br/index.php/relatoriodebalneabilidade/boletim']
+
+    def parse(self, response):
+        for campanha in response.xpath('//select[@id="idcampanha"]/option'):
+            id_campanha = campanha.xpath('./@value').extract()[0]
+            if id_campanha == '-1':  # 'TODAS AS CAMPANHAS'
+                continue
+            partes = (campanha.xpath('./text()').extract()[0]
+                              .strip()
+                              .replace('CAMPANHA EMERG', 'CAMPANHA_EMERG')
+                              .split())
+            partes_data = [int(parte) for parte in partes[-1].split('/')]
+            data = f'{partes_data[2]}-{partes_data[1]:02d}-{partes_data[2]:02d}'
+
+            yield {
+                'costa': partes[2],
+                'data_emissao': data,
+                'id_campanha': id_campanha,
+                'numero_boletim': partes[1],
+                'tipo_campanha': partes[0],
+                'url': URL_BOLETIM.format(id_campanha=id_campanha),
+            }
